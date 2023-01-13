df2fc35e634dc1f8a6650ba4874ac20adc644288
Dio Lantief Widoyoko <dio_lantief21@outlook.com>
2023-01-13 19:14:15 +0700

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,15 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Cable;
-use App\Models\Multi;
-use App\Models\Point;
+use App\Models\Fo;
+use App\Models\Witel;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
 use Illuminate\Http\Request;
-use App\Models\User;
+// use App\Models\User;
 
 class Controller extends BaseView
 {
@@ -18,41 +17,9 @@ class Controller extends BaseView
 
     public function index()
     {
-        $this->data['cables'] = Cable::all();
+        $this->data['witels'] = Witel::all();
+        $this->data['fos'] = Fo::all();
         return $this->render('index');
     }
 
-    public function cable(string $cableID) {
-        $cable = Cable::find($cableID);
-        $this->data['cable'] = $cable;
-        
-        $coords = json_decode($cable->line->toJson())->coordinates;
-        $lat = 0;
-        $lng = 0;
-        foreach($coords as $latlng) {
-            $lat += $latlng[0];
-            $lng += $latlng[1];
-        }
-        $count = count($coords);
-        $this->data['centerView'] = [$lat/$count, $lng/$count];
-
-        return $this->render('cable');
-    }
-
-    public function addPoint(Request $req, $pointID) {
-        $data = $req->all();
-        $data['multi_id'] = $pointID;
-        $point = new Point();
-        $id = $point->insertGetId($data);
-        return response()->json(Point::find($id), 201);
-    }
-
-    public function deletePoint($pointID) {
-        $del = Point::where('multi_id', $pointID)->latest('id')->first();
-        $del->forceDelete();
-        return response()->json($del, 204);
-    }
-
-    public function v3() {
-    }
 }
