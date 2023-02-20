c42b7e287e209c11fd2bbed535209bdd61ef52be
Dio Lantief Widoyoko <dio_lantief21@outlook.com>
2023-02-21 04:04:03 +0700

    Update root.blade.php

--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
 
     <?php if (env('APP_ENV') == 'local'):?>
     <title>Laraflet Debug</title>
@@ -56,6 +56,13 @@
     <!-- JQuery -->
     <script src="https://code.jquery.com/jquery-3.6.3.min.js"
         integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+    <script type="text/javascript">
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+    </script>
 
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/@materializecss/materialize@1.2.1/dist/js/materialize.min.js"></script>
